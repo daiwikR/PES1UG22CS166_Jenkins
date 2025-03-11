7b24b2011823fd26a26ca041cf3a755a3d03e73e
DaiwikR <daiwik.bhasker04@gmail.com>
2025-03-11 16:28:24 +0530

    Test webhook automation

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,13 +1,5 @@
-//File: hello.cpp
 #include <iostream>
-using namespace std;
-
-int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
-  cout<<"Rayansh College of hotel management"<<endl;
-  cout<<"<3"<<endl;
-  cout<<"</3"<<endl;
-  return 0;
+int main() {
+    std::cout << "Hello from Jenkins Webhook!" << std::endl;
+    return 0;
 }
