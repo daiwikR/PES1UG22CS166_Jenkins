319a85e08f987c08d00b06b6381f74eff536afa9
DaiwikR <daiwik.bhasker04@gmail.com>
2025-03-11 16:39:45 +0530

    made error

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 int main() {
     std::cout << "Hello from Jenkins Webhook!" << std::endl;
-    std::cout << "Hello from Jenkins Webhook!" << std::endl;
+    std::count << "Hello from Jenkins Webhook!" << std::endl;
     return 0;
 }
