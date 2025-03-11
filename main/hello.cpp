bbd43a13afe3a804746c246fcf910048dc334a4b
DaiwikR <daiwik.bhasker04@gmail.com>
2025-03-11 15:43:25 +0530

    added new print statement

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  count<<"Rayansh College of hotel management"<<endl;
   return 0;
 }
